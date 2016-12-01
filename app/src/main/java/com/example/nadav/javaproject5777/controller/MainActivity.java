25ff7c106ee1e403092a81201b741992fa646e98
naweiss <nadavw2@gmail.com>
2016-12-01 12:31:55 +0200

    Second Commit Message

--- app/src/main/java/com/example/nadav/javaproject5777/controller/MainActivity.java
+++ app/src/main/java/com/example/nadav/javaproject5777/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.nadav.javaproject5777;
+package com.example.nadav.javaproject5777.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.nadav.javaproject5777.R;
 
 public class MainActivity extends AppCompatActivity {
 
