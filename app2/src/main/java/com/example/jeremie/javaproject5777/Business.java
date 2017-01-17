2f9cf8b22c8d233618bf3ad1dee6bfee34c02b7d
naweiss <nadavw2@gmail.com>
2017-01-17 14:34:29 +0200

    App2 was added

---
+++ app2/src/main/java/com/example/jeremie/javaproject5777/Business.java
@@ -0,0 +1,80 @@
+package com.example.jeremie.javaproject5777;
+
+import java.net.URL;
+
+/**
+ * Created by nadav on 12/25/2016.
+ * Package: com.example.nadav.testproject
+ */
+
+public class Business {
+
+    private static int count =0;
+    private int id;
+    private String name;
+    private Address address;
+    private String phone;
+    private String email;//^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]+)$
+    private URL link;
+
+    public Business() {
+
+    }
+
+    public Business(Address address, String email, URL link, String name, String phone) {
+        this.id = count++;
+        this.address = address;
+        this.email = email;
+        this.link = link;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public URL getLink() {
+        return link;
+    }
+
+    public void setLink(URL link) {
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ app2/src/main/java/com/example/jeremie/javaproject5777/Fragments/MainFragment.java
@@ -0,0 +1,54 @@
+package com.example.jeremie.javaproject5777.Fragments;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jeremie.javaproject5777.Address;
+import com.example.jeremie.javaproject5777.Business;
+import com.example.jeremie.javaproject5777.MyAdapter;
+import com.example.jeremie.javaproject5777.R;
+
+import java.net.URL;
+
+public class MainFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(view.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        try {
+            // specify an adapter (see also next example)
+            Business[] myDataset = new Business[]
+                    {new Business(new Address("Israel", "Hifa", "Arlozorov", 031231), "avi_levi@gmail.com", new URL("http://www.google.com"), "avi levi", "056-712-3123")
+                            , new Business(new Address("Israel", "Jerusalem", "Arlozorov", 031231), "avi@gmail.com", new URL("http://stackoverflow.com"), "avi cohen", "056-745-6456")
+                            , new Business(new Address("Israel", "Hifa", "Hamacabim", 013213), "david123@jct.ac.il", new URL("http://www.123.456.com"), "david malcha", "050-567-9789")
+                            ,new Business(new Address("Israel", "Hifa", "Arlozorov", 031231), "avi_levi@gmail.com", new URL("http://www.google.com"), "avi levi", "056-712-3123")
+                            ,new Business(new Address("Israel", "Hifa", "Arlozorov", 031231), "avi_levi@gmail.com", new URL("http://www.google.com"), "avi levi", "056-712-3123")
+                            ,new Business(new Address("Israel", "Hifa", "Arlozorov", 031231), "avi_levi@gmail.com", new URL("http://www.google.com"), "avi levi", "056-712-3123")};
+            mAdapter = new MyAdapter(myDataset,view.getContext());
+            mRecyclerView.setAdapter(mAdapter);
+        }catch (Exception e){}
+        return view;
+    }
+}
